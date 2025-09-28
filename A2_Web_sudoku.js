f9d47e9d27e21d9d736955f7fe2dd445cceac0ff
nakr0p <s6801012610294@email.kmutnb.ac.th>
2025-09-29 01:52:56 +0700

    Create selected variable / Build pick_number function

--- A2_Web_sudoku.js
+++ A2_Web_sudoku.js
@@ -7,6 +7,8 @@ let selectNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let size = 800;
 let distanceBetweenLine = size / 9;
 
+let selected = 0;
+
 let x; 
 let y;
 let xStart;
@@ -14,16 +16,32 @@ let yStart;
 
 function setup() {
     createCanvas(displayWidth, displayHeight); 
-
     textAlign(CENTER, CENTER);
 
     x = (displayWidth / 2) - (size / 2);
     y = (displayHeight / 2) - (size / 2);
-
-    xStart = (displayWidth / 2) - (size / 2);
-    yStart = (displayHeight / 2) - (size / 2);
+    xStart = x;
+    yStart = y;
+    
+    textSize(distanceBetweenLine * 0.6); 
 }
 
 function draw() {
-    
+    background(255); 
+
+    drawSudokuTable(xStart, yStart, distanceBetweenLine, size);
+    drawNumber(xStart, yStart, distanceBetweenLine);
+    drawSelect(xStart, yStart, distanceBetweenLine);
 }
+
+function drawSudokuTable(x, y, d, s) {
+    strokeWeight(1);
+    for (let i = 0; i < 10; i++) {
+        line(x + i * d, y, x + i * d, y + s);
+        line(x, y + i * d, x + s, y + i * d);
+    }
+    
+    strokeWeight(4);
+    for (let i = 0; i < 4; i++) {
+        line(x + i * 3 * d, y, x + i * 3 * d, y + s);
+        line(x, y + i *
